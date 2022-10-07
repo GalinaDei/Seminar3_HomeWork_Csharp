d9572d11d0b9e07ff0488bd7f4f6a683e8c070b5
Galina <gal-dej@yandex.ru>
2022-10-07 16:04:13 +0300

    Task21 complited

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -2,4 +2,13 @@
 Напишите программу, которая принимает на вход координаты двух точек и 
 находит расстояние между ними в 3D пространстве.
 A (3,6,8); B (2,1,-7), -> 15.84
-A (7,-5, 0); B (1,-1,9) -> 11.53*/
+A (7,-5, 0); B (1,-1,9) -> 11.53
+Distance (d) = Sqrt((xA-xB)^2+(yA-yB)^2+(zA-zB)^2) 
+*/
+void GetDistance(double xA, double xB, double yA, double yB, double zA, double zB)
+{
+    double result = 0;
+    result = Math.Sqrt (Math.Pow((xA-xB),2)+Math.Pow((yA-yB),2)+Math.Pow((zA-zB),2));
+    Console.WriteLine ($"A ({xA},{yA},{zA},); B({xB},{yB},{zB}) -> {result}");
+}
+GetDistance(3, 2, 6, 1, 8, -7);
