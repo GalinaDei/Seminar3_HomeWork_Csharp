913fdc456f66f7937218869fd2a6277e5a8eebcd
Galina <gal-dej@yandex.ru>
2022-10-07 15:25:53 +0300

    Task complited

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -5,6 +5,14 @@
 23432 -> да */
 void PalindromSearch (int number)
 {
-    
+    string num = number.ToString();
+    if (num[0]==num[4] & num[1]==num[3])
+    {
+        Console.WriteLine($"{number} -> да");
+    }
+    else 
+    {
+        Console.WriteLine($"{number} -> нет");
+    }
 }
-PalindromSearch (12267);
+PalindromSearch (12921);
