5f606ad4446c81547fd55a224ba108931d76983f
Galina <gal-dej@yandex.ru>
2022-10-07 16:17:23 +0300

    Task23 complited

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -3,3 +3,16 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+void NumСube (double N)
+{
+    Console.Write($"{N} -> ");
+    int i = 1;
+    for (i=1; i< N; i++)
+    {
+        double result = Math.Pow(i,3);
+        Console.Write($"{result},");
+    }
+    double numCube = N*N*N;
+    Console.WriteLine($"{numCube}.");
+}
+NumСube (5);
